34b80ef407ed14c16d68c4dcf1b24cacbbfb11b6
azaruddhin khan <khanazharuddin740@gmail.com>
2018-07-23 15:16:43 +0530

    emp file

---
+++ Employee.py
@@ -0,0 +1,15 @@
+class Employee:
+	def __init__(self,first,last,pay):
+		self.first=first
+		self.last=last
+		self.pay=pay
+	@property
+	def email(self):
+		return '{}.{}.@email.com'.format(self.first,self.last)
+	@property
+	def fullname(self):
+                return '{} {}'.format(self.first,self.last)
+
+emp1=Employee('azhar','khan')
+print(emp1.email())
+~                                                             
